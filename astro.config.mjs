eaa31ee24d1d3dec41afed231afab7cc415067d9
Satindar <ibrahimasif711@outlook.com>
2024-07-17 18:05:06 +0530

    basic design

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://satindar31.github.io",
-    integrations: [tailwind()],
+  site: "https://satindar31.github.io",
+  integrations: [tailwind(), icon()]
 });
