e9fea08b75108d0e928373bfea84a95cfbe80627
Satindar <ibrahimasif711@outlook.com>
2024-07-17 13:34:46 +0530

    coming soon text

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://satindar31.github.io"
+    site: "https://satindar31.github.io",
+    integrations: [tailwind()],
 });
